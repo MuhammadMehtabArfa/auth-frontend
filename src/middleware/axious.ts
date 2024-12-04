3343e334911d1ab199e939fa0260268a825712d5
MUHAMMADMEHTABARFA <muhammadmehtabarfa@gmail.com>
2024-12-04 13:04:19 +0500

    axios

---
+++ src/middleware/axious.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/", 
+});
+
+
+
+
+
+
+
+
+
+export default apiClient;
